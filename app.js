2e3697968015ca8eb7fa1d134e77973a69f607ca
AlanMandriaza <alan.mandriaza@gmail.com>
2023-02-24 22:44:38 +0000

    terminado

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 const expresiones = {
-  
+
   nombre: /^[a-zA-ZÀ-ÿ\s]{1,15}$/,
   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
   cvc: /^[0-9]{1,3}$/,
   card: /^[0-9]{12}$/,
   cash: /^[0-9]{1,8}$/,
+  textarea: /^.{1,90}$/,
 }
 
 
@@ -31,21 +32,27 @@ const enviarFormulario = (e) => {
   /*===============================*/
   const inputcvc = document.getElementById('cvc').value;
   const cvcNumber = document.getElementById('cvc');
-   /*===============================*/
-   const inputcashAmount = document.getElementById('cashAmount').value;
-   const cashAmount = document.getElementById('cashAmount');
+  /*===============================*/
+  const inputcashAmount = document.getElementById('cashAmount').value;
+  const cashAmount = document.getElementById('cashAmount');
+  /*===============================*/
+  const state = document.getElementById('state');
+  /*===============================*/
+  const inputemail = document.getElementById('email').value;
+  const email = document.getElementById('email');
+  /*===============================*/
+  const inputmessage = document.getElementById('message').value;
+  const message = document.getElementById('message');
 
-
-  
 
   var mensajesError = 0;
 
   /*=============NOMBRE==================*/
- 
+
   if (expresiones.nombre.test(inputfName) === false) {
     fName.classList.add('bg-custom');
     mensajesError++;
-   
+
   } else {
     fName.classList.remove('bg-custom');
   }
@@ -94,7 +101,30 @@ const enviarFormulario = (e) => {
   } else {
     cashAmount.classList.remove('bg-custom');
   }
+  /*=============state==================*/
 
+  if (state.value === "a") {
+    state.classList.add('bg-custom2');
+  } else {
+    state.classList.remove('bg-custom2');
+  }
+  /*=================email==============*/
+
+  if (expresiones.correo.test(inputemail) === false) {
+    email.classList.add('bg-custom');
+    mensajesError++;
+  } else {
+    email.classList.remove('bg-custom');
+  }
+
+  /*=================textarea==============*/
+
+  if (expresiones.textarea.test(inputmessage) === false) {
+    message.classList.add('bg-custom');
+    mensajesError++;
+  } else {
+    message.classList.remove('bg-custom');
+  }
   /*==============clase=error=========*/
 
   if (mensajesError > 0) {
